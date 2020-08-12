aa44cf975e5260c931adf94da6d110b7fa8f3cb1
danielle-harada <danielle.harada@hotmail.com>
2020-08-12 16:31:03 -0300

    Corrigindo edição de produto

--- editProduto.php
+++ editProduto.php
@@ -9,73 +9,77 @@ include 'header.php';
 
 $id = $_GET['id'];
 $extensoesValidas = ['image/jpeg','image/png', 'image/jpg'];
-$salvo;
-$msg="";
-
+$salvo=false;
+$msg='';
 
 $query = $db->prepare ("SELECT *
-                        FROM produtos
-                        WHERE id=:id;");
-$query->execute([':id' => $id]);
+  FROM produtos
+  WHERE id=:id;");
+  $query->execute([':id' => $id]);
 
-$produtos=$query->fetchAll(PDO::FETCH_ASSOC);
+  $produtos=$query->fetchAll(PDO::FETCH_ASSOC);
 
-if ($_POST){
-  if ($_FILES['foto']['name'] != 0) { //verifica se tem files
-    if ($_FILES['foto']['error'] == 0) { //verifica se não tem erro
-      if (array_search($_FILES['foto']['type'], $extensoesValidas) == false) { //valida as extensoes
-          $msg="Extensão do arquivo inválida!<br>";}
+  if ($_POST){
+    if ($_FILES['foto']['name'] != "") {//verifica se tem files
+      if ($_FILES['foto']['error'] == 0) {//verifica se não tem erro
+        if (array_search($_FILES['foto']['type'], $extensoesValidas) == false) { //valida as extensoes
+          $msg="Extensão do arquivo inválida!<br>";
+        }
         else {
-        move_uploaded_file($_FILES['foto']['tmp_name'],'../DesafioPHPSQL/Imagens/'.$id.'.jpg');
-        $query = $db-> prepare("UPDATE produtos SET   nome = :nome,
+          move_uploaded_file($_FILES['foto']['tmp_name'],'../DesafioPHPSQL/Imagens/'.$id.'.jpg');
+          $query = $db-> prepare("UPDATE produtos SET nome = :nome,
                                                       descricao = :descricao,
                                                       preco = :preco,
                                                       foto = :foto
-                                                WHERE id = :id;");
-        $salvo=$query->execute([':id' => $id,
-                                ':nome'=>$_POST['nomeProduto'],
-                                ':descricao'=>$_POST['descricao'],
-                                ':preco'=>$_POST['preco'],
-                                ':foto'=>$_FILES['foto']['name']]);}}//se válida, move para a pasta Imagens
-      } else {
+                                                  WHERE id = :id;");
+            $query->execute([':id' => $id,
+                             ':nome'=>$_POST['nomeProduto'],
+                             ':descricao'=>$_POST['descricao'],
+                             ':preco'=>$_POST['preco'],
+                             ':foto'=>$_FILES['foto']['name']]);
+            header ('Location: http://localhost/projetos/DesafioPHPSQL/indexProdutos.php');
+          }
+        }
+        else {
+          $msg="Erro no envio do arquivo!";
+        }
+      }
+      else {
         $query = $db-> prepare("UPDATE produtos SET nome = :nome,
                                                     descricao = :descricao,
                                                     preco = :preco
-                                              WHERE id = :id;");
-        $salvo=$query->execute([':id' => $id,
-                                ':nome'=>$_POST['nomeProduto'],
-                                ':descricao'=>$_POST['descricao'],
-                                ':preco'=>$_POST['preco']]);}}//se válida, move para a pasta Imagens}
+                                                WHERE id = :id;");
+        $query->execute([':id' => $id,
+                         ':nome'=>$_POST['nomeProduto'],
+                         ':descricao'=>$_POST['descricao'],
+                         ':preco'=>$_POST['preco']]);
+        header ('Location: http://localhost/projetos/DesafioPHPSQL/indexProdutos.php');
+        }
+      }
+      ?>
 
-// var_dump($_POST);
-// var_dump($_FILES);
-if(isset($salvo)){
-  header ('Location: http://localhost/projetos/DesafioPHPSQL/indexProdutos.php');}
-
-?>
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Editar Produto</title>
-  </head>
-  <body>
-    <?php foreach ($produtos as $produto): ?>
-    <form class="" method="post" enctype="multipart/form-data">
-      <label for="nomeProduto">Nome do Produto</label><br>
-       <input type="text" name="nomeProduto" value="<?= $produto['NOME']?>" required><br>
-       <label for="descricao">Descrição</label><br>
-       <input type="text" name="descricao" value="<?= $produto['DESCRICAO']?>"><br>
-      <label for="preco">Preço</label><br>
-       <input type="number" name="preco" min="0" step=".01" value="<?= $produto['PRECO']?>"><br>
-      <label for="">Foto</label><br>
-      <img src="../DesafioPHPSQL/Imagens/<?=$id;?>.jpg" alt="">
-       <input type="file" name="foto"><br>
-       <?php echo $msg;?>
-       <input type="hidden" name="id" value="<?= $id ?>">
-       <input type="submit" name="button" value="Atualizar Produto">
-    </form>
-    <?php endforeach; ?>
-  </body>
-</html>
+      <!DOCTYPE html>
+      <html lang="en" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title>Editar Produto</title>
+      </head>
+      <body>
+        <?php foreach ($produtos as $produto): ?>
+          <form class="" method="post" enctype="multipart/form-data">
+            <label for="nomeProduto">Nome do Produto</label><br>
+            <input type="text" name="nomeProduto" value="<?= $produto['NOME']?>" required><br>
+            <label for="descricao">Descrição</label><br>
+            <input type="text" name="descricao" value="<?= $produto['DESCRICAO']?>"><br>
+            <label for="preco">Preço</label><br>
+            <input type="number" name="preco" min="0" step=".01" value="<?= $produto['PRECO']?>"><br>
+            <label for="">Foto</label><br>
+            <img src="../DesafioPHPSQL/Imagens/<?=$id;?>.jpg" alt="">
+            <input type="file" name="foto"><br>
+            <?php echo $msg;?>
+            <input type="hidden" name="id" value="<?= $id ?>">
+            <input type="submit" name="button" value="Atualizar Produto">
+          </form>
+        <?php endforeach; ?>
+      </body>
+      </html>
